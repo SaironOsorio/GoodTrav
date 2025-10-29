15d1dfc3ddd31eccd6061d39cf7ccea94d4b7739
Sosorioo <Osoriosairon244@gmail.com>
2025-10-29 12:05:51 -0500

    Add country code to users and registration form
    Introduced a new 'country_code' column to the users table via migration. Updated the registration form to include a country code select dropdown alongside the phone number input.

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -29,14 +29,49 @@
             />
 
             <!-- Phone Number -->
-            <flux:input
-                name="phone"
-                :label="__('Phone number')"
-                type="tel"
-                required
-                autocomplete="tel"
-                :placeholder="__('Phone number')"
-            />
+            <div class="flex gap-2">
+                <flux:select
+                    name="country_code"
+                    :label="__('Code')"
+                    required
+                    class="w-32"
+                >
+                    <option value="+1">+1 Estados Unidos / Canadá</option>
+                    <option value="+44">+44 Reino Unido</option>
+                    <option value="+34" selected>+34 España</option>
+                    <option value="+49">+49 Alemania</option>
+                    <option value="+33">+33 Francia</option>
+                    <option value="+39">+39 Italia</option>
+                    <option value="+52">+52 México</option>
+                    <option value="+57">+57 Colombia</option>
+                    <option value="+51">+51 Perú</option>
+                    <option value="+54">+54 Argentina</option>
+                    <option value="+55">+55 Brasil</option>
+                    <option value="+58">+58 Venezuela</option>
+                    <option value="+41">+41 Suiza</option>
+                    <option value="+31">+31 Países Bajos</option>
+                    <option value="+61">+61 Australia</option>
+                    <option value="+81">+81 Japón</option>
+                    <option value="+82">+82 Corea del Sur</option>
+                    <option value="+86">+86 China</option>
+                    <option value="+91">+91 India</option>
+                    <option value="+7">+7 Rusia / Kazajistán</option>
+                    <option value="+60">+60 Malasia</option>
+                    <option value="+65">+65 Singapur</option>
+                    <option value="+66">+66 Tailandia</option>
+
+                </flux:select>
+
+                <flux:input
+                    name="phone"
+                    :label="__('Phone number')"
+                    type="tel"
+                    required
+                    autocomplete="tel"
+                    :placeholder="__('Phone number')"
+                    class="flex-1"
+                />
+            </div>
 
             <!-- Date of Birth -->
             <flux:input
