db88120c069acd953a27f8142af7b042ed190bec
Sosorioo <Osoriosairon244@gmail.com>
2025-11-19 13:29:52 -0500

    Add montserrat-regular class to trips subtitle
    Applied the 'montserrat-regular' class to the subtitle in the trips view for improved typography consistency.

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app :title="__('Trips')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <h1 class="font-bold text-4xl text-[#5170ff]">{{ __('Trips') }}</h1>
-        <h2 class="font-light text-2xl">{{ __('Explore the world with GoodTrav') }}</h2>
+        <h2 class="font-light text-2xl montserrat-regular">{{ __('Explore the world with GoodTrav') }}</h2>
         <div>
               @livewire('tripinfo')
         </div>
