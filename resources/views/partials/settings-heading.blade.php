00f971768a00f4846db13b0a1628de0544907292
Sosorioo <Osoriosairon244@gmail.com>
2025-11-21 17:34:24 -0500

    Replace subheading component with h2 in settings heading
    Replaces the <flux:subheading> component with a standard <h2> element for the profile and account settings description, updating classes for styling consistency.

--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,5 +1,5 @@
 <div class="relative mb-6 w-full">
     <flux:heading class="poppins-extrabold text-[#5170ff] text-4xl" size="xl" level="1">{{ __('Info & Help') }}</flux:heading>
-    <flux:subheading class="text-2xl montserrat-medium text-black" size="lg" class="mb-6">{{ __('Administra la configuración de tu perfil y cuenta') }}</flux:subheading>
+    <h2 class="font-light text-2xl montserrat-regular mb-4">{{ __('Administra la configuración de tu perfil y cuenta') }}</h2>
     <flux:separator variant="subtle" />
 </div>
